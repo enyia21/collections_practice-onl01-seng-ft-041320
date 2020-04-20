8d53527df35d3adbf4745bf93ea69cbe9a790d16
Chibuisi Enyia <enyia21@gmail.com>
2020-04-20 16:05:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,3 +46,7 @@ end
 def sum_array(array)
   array.inject{|sum, n| sum + n }
 end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index| index == 1? "#{element}" : "#{element}s" }
+end
