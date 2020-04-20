f6b6943682bafcfa52848a984063285d47f6fa87
Chibuisi Enyia <enyia21@gmail.com>
2020-04-20 14:09:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  array = array.sort do |a, b| b<=>a end
+  return array
+end
+
+def sort_array_char_count(array)
+   array.sort_by!{|x| x.length}
+   return array
+end
+
+def swap_elements(array)
+  temp = ""
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  return array
+end
